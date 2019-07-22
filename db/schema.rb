fca556e31e6a14f5f719c2369d6cc33944cfbb0b
bigkeios <cqphuong97@gmail.com>
2019-07-22 14:01:59 +0700

    Finish create and show user with error when signup

--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_07_22_081656) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.string "string"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
