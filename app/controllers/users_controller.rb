985c10bcabddbd4934f8c50dcba4359371d2d61a
bigkeios <cqphuong97@gmail.com>
2019-07-23 15:51:31 +0700

    Add admin role and delete user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only:[:index, :edit, :update]
+  before_action :logged_in_user, only:[:index, :edit, :update, :destroy]
   before_action :correct_user, only:[:edit, :update]
+  before_action :admin_user, only:[:destroy]
 
   #GET users
   def index
@@ -48,6 +49,13 @@ class UsersController < ApplicationController
     end
   end
 
+  # DELETE users/1
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "Delete user successfully"
+    redirect_to users_url
+  end
+
   private
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
@@ -67,5 +75,10 @@ class UsersController < ApplicationController
     def correct_user
       @user = User.find(params[:id])
       redirect_to root_url unless correct_user?(@user)
+    end
+
+    # method to make sure it is admin making the action
+    def admin_user
+      redirect_to root_url unless current_user.admin?
     end
 end
