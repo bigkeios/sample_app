c3ef257435fab8f86de2c4aa2a878a040ae9ba91
bigkeios <cqphuong97@gmail.com>
2019-07-25 18:14:49 +0700

    Add method for follow and unfollow

---
+++ app/controllers/relationships_controller.rb
@@ -0,0 +1,15 @@
+class RelationshipsController < ApplicationController
+    before_action :logged_in_user
+    # POST /relationships
+    def create
+        user = User.find_by(id: params[:followed_id])
+        current_user.follow(user)
+        redirect_to user_url(user)
+    end
+    # DELETE /relationships/1
+    def destroy
+        user = Relationship.find_by(id: params[:id])
+        current_user.unfollow(user)
+        redirect_to user_url(user)
+    end
+end
