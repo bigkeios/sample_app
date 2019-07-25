ccf529b16eb5d894c737d127352e4634707e2c45
bigkeios <cqphuong97@gmail.com>
2019-07-25 14:08:16 +0700

    Add destroy MPosts

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,6 @@
 class MicropostsController < ApplicationController
-    before_action :logged_in_user, only: [:create, :update]
+    before_action :logged_in_user, only: [:create, :update, :destroy]
+    before_action :correct_user, only: [:destroy]
 
     # POST /microposts
     def create
@@ -8,7 +9,7 @@ class MicropostsController < ApplicationController
             flash[:success] = "New micropost created"
             redirect_to root_path
         else
-            # @feed_items = []
+            @feed_items = Micropost.feed(current_user.id).paginate(page: params[:page])
             render 'static_pages/home'
         end
     end
@@ -16,8 +17,19 @@ class MicropostsController < ApplicationController
     def update
     end
 
+    # DELETE /microposts/1
+    def destroy
+        @micropost.destroy
+        flash[:success] = "Micropost deleted"
+        redirect_to request.referrer || root_url
+    end
     private
     def micropost_params
         params.require(:micropost).permit(:content)
+    end
+    
+    def correct_user
+        @micropost = current_user.microposts.find_by(id: params[:id])
+        redirect_to root_url if @micropost.nil?
     end
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,11 @@ class StaticPagesController < ApplicationController
     #an empty micropost for the form to post new post
     @micropost = current_user.microposts.build if logged_in?
     # lists of posts by the current user
-    @feed_items = Micropost.feed(current_user.id).paginate(page: params[:page])
+    if logged_in?
+      @feed_items = Micropost.feed(current_user.id).paginate(page: params[:page])
+    else
+      @feed_items = []
+    end
   end
 
   def help
